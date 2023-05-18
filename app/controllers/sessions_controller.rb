a56f5844a61e5bc94d3e307cf866a0ec2bf651b5
Christina DiMaggio <christina.dimaggio@kellogg.northwestern.edu>
2023-05-18 02:07:59 +0000

    end of class code alongs

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,16 +5,25 @@ class SessionsController < ApplicationController
 
   def create
     # authenticate the user
-    # 1. try to find the user by their unique identifier
-    # 2. if the user exists -> check if they know their password
-    # 3. if they know their password -> login is successful
-    # 4. if the email does not exist or they do not know their password -> login fails
-    flash["notice"] = "Nope."
-    redirect_to "/sessions/new"
+    @user = User.find_by({"email" => params["email"]})
+    if @user != nil
+      if BCrypt::Password.new(@user["password"]) == params["password"]
+        session["user_id"] =@user["id"]
+        redirect_to "/companies"
+        flash["notice"] = "Welcome, #{@user["first_name"]}!"
+      else    
+        flash["notice"] = "Nope."
+        redirect_to "/sessions/new" 
+      end
+    else
+      flash["notice"] = "Nope."
+      redirect_to "/sessions/new"
+    end
   end
 
   def destroy
     # logout the user
+    session["user_id"] = nil # we have to end the cookies
     flash["notice"] = "Goodbye."
     redirect_to "/sessions/new"
   end
